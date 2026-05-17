0e0fe439f92b0199d7b98598475c318be47013f2
Devanshu Garg <mrdevanshugarg@gmail.com>
2026-05-17 14:18:53 +0530

    Added frontend routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import DashboardPage from "./pages/DashboardPage";
 
+
 function App() {
-  return <DashboardPage />;
+  return (
+    <Routes>
+
+      <Route
+        path="/login"
+        element={<LoginPage />}
+      />
+
+      <Route
+        path="/register"
+        element={<RegisterPage />}
+      />
+
+      <Route
+        path="/"
+        element={<DashboardPage />}
+      />
+
+    </Routes>
+  );
 }
 
 export default App;
